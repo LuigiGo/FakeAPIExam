45a9bcc69d85509ebee833444efc66f8d1682f8a
LuigiGo <luigee.8@gmail.com>
2024-04-06 01:12:04 +0800

    [Implementation] Implement loading of environment class in main

--- lib/core/main.dart
+++ lib/core/main.dart
@@ -1,5 +1,11 @@
+import 'package:fake_api_exam/core/utils/environments.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
 
-void main() => runApp(const MyApp());
+void main() async {
+  String flavor = const String.fromEnvironment('app_flavor');
+  await Environment().initializeEnvironment(flavor);
+
+  runApp(const MyApp());
+}
