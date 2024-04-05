89408377b6bc3c675cbc463c55f036644731240b
LuigiGo <luigee.8@gmail.com>
2024-04-06 05:00:37 +0800

    [Presentation] Implement dependency in the Homepage using BlocProvider

--- lib/features/homepage/presentation/screens/homepage.dart
+++ lib/features/homepage/presentation/screens/homepage.dart
@@ -1,11 +1,8 @@
-import 'package:fake_api_exam/features/homepage/data/repositories/HomepageRepositoryImpl.dart';
-import 'package:fake_api_exam/features/homepage/data/repositories/datasourceimpl/persons_remote_datasource_impl.dart';
-import 'package:fake_api_exam/features/homepage/domain/usecases/get_list_of_persons_usecase.dart';
 import 'package:fake_api_exam/features/homepage/presentation/blocs/homepage_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../../../core/network/dio_client.dart';
+import '../../../../core/di/injection.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -17,56 +14,25 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  late DioClient _client;
-  late PersonsRemoteDatasourceImpl personsRemoteDatasourceImpl;
-  late HomepageRepositoryImpl homepageRepositoryImpl;
-  late GetListOfPersonsUseCase getListOfPersonsUseCase;
-
-  String _strResponse = '';
-
-  @override
-  void initState() {
-    _client = DioClient();
-    personsRemoteDatasourceImpl = PersonsRemoteDatasourceImpl(client: _client);
-    homepageRepositoryImpl = HomepageRepositoryImpl(
-        personRemoteDatasource: personsRemoteDatasourceImpl);
-    getListOfPersonsUseCase =
-        GetListOfPersonsUseCase(repository: homepageRepositoryImpl);
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: [
         BlocProvider(create: (context) {
-          return HomepageCubit(getListOfPersonsUseCase);
+          return HomepageCubit(
+            getListOfPersonsUseCase: inject(),
+          );
         }),
       ],
-      child: MultiBlocListener(
-        listeners: [
-          BlocListener<HomepageCubit, HomepageState>(
-            listener: (context, state) {
-              if (state is HomepageInitial) {
-                print(state);
-              } else if (state is LoadListOfPersonSuccess) {
-                print('success: ${state.persons}');
-                _strResponse = state.persons.toString();
-              }
-            },
-          ),
-        ],
-        child: BlocBuilder<HomepageCubit, HomepageState>(
-          builder: (context, state) {
-            context.read<HomepageCubit>().getListOfPersons(20);
-            return Scaffold(
-              appBar: AppBar(
-                backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-                title: Text(widget.title),
-              ),
-              body: Center(
-                child: Text(_strResponse),
-              ),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: ListView.builder(
+          itemBuilder: (_, i) {
+            return ListTile(
+              title: Text('$i'),
             );
           },
         ),
