ee086c45886035d2cc30336d6eafde9a0b64f196
LuigiGo <luigee.8@gmail.com>
2024-04-06 01:47:20 +0800

    [Network] Implement KeyConst in DioClient

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,11 +1,24 @@
 import 'package:dio/dio.dart';
+import 'package:fake_api_exam/core/const/keyconst.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class DioClient {
   late Dio _dio;
 
   DioClient() {
-    BaseOptions options = BaseOptions();
+    String baseUrl = dotenv.get(
+      KeyConst.baseUrl,
+      fallback: 'Undefined Base URL',
+    );
+
+    BaseOptions options = BaseOptions(
+      baseUrl: baseUrl,
+    );
 
     _dio = Dio(options);
+  }
+
+  Dio getDio() {
+    return _dio;
   }
 }
