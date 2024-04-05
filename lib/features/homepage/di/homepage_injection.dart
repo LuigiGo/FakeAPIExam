35cf9c89f25f6bd04591288d18745e8b294b9a1a
LuigiGo <luigee.8@gmail.com>
2024-04-06 04:58:52 +0800

    [DI] Create homepage injection container - this file handled the initiation of all dependency for Homepage

---
+++ lib/features/homepage/di/homepage_injection.dart
@@ -0,0 +1,23 @@
+import 'package:fake_api_exam/features/homepage/data/repositories/HomepageRepositoryImpl.dart';
+import 'package:fake_api_exam/features/homepage/data/repositories/datasource/person_remote_datasource.dart';
+import 'package:fake_api_exam/features/homepage/data/repositories/datasourceimpl/persons_remote_datasource_impl.dart';
+import 'package:fake_api_exam/features/homepage/domain/repositories/homepage_repository.dart';
+import 'package:fake_api_exam/features/homepage/domain/usecases/get_list_of_persons_usecase.dart';
+
+import '../../../core/di/injection.dart';
+
+class HomepageInjectionContainer {
+  Future<void> initHomepageInjector() async {
+    /// Datasource
+    inject.registerLazySingleton<PersonRemoteDatasource>(
+        () => PersonsRemoteDatasourceImpl(client: inject()));
+
+    /// Repository
+    inject.registerLazySingleton<HomepageRepository>(
+        () => HomepageRepositoryImpl(personRemoteDatasource: inject()));
+
+    /// Usecase
+    inject.registerLazySingleton(
+        () => GetListOfPersonsUseCase(repository: inject()));
+  }
+}
