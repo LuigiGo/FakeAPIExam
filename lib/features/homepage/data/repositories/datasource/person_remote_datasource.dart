87780432768d6ce832bd53f34ca97d41597e2a00
LuigiGo <luigee.8@gmail.com>
2024-04-06 04:30:44 +0800

    [DataSource] Implement either to handle api response in datasource

--- lib/features/homepage/data/repositories/datasource/person_remote_datasource.dart
+++ lib/features/homepage/data/repositories/datasource/person_remote_datasource.dart
@@ -1,3 +1,7 @@
+import 'package:either_dart/either.dart';
+import 'package:fake_api_exam/core/network/exceptions/server_error.dart';
+import 'package:fake_api_exam/features/homepage/data/models/api_response.dart';
+
 abstract class PersonRemoteDatasource {
-  Future getListOfPersons(int quantity);
+  Future<Either<ServerError, ApiResponse>> getListOfPersons(int quantity);
 }
--- lib/features/homepage/data/repositories/datasourceimpl/persons_remote_datasource_impl.dart
+++ lib/features/homepage/data/repositories/datasourceimpl/persons_remote_datasource_impl.dart
@@ -1,17 +1,37 @@
+import 'package:either_dart/either.dart';
+import 'package:fake_api_exam/core/base/base_repository_impl.dart';
 import 'package:fake_api_exam/core/const/apiconst.dart';
+import 'package:fake_api_exam/core/const/keyconst.dart';
 import 'package:fake_api_exam/core/network/dio_client.dart';
+import 'package:fake_api_exam/core/network/exceptions/server_error.dart';
 
+import '../../models/api_response.dart';
 import '../datasource/person_remote_datasource.dart';
 
-class PersonsRemoteDatasourceImpl extends PersonRemoteDatasource {
+class PersonsRemoteDatasourceImpl extends PersonRemoteDatasource
+    with BaseRepositoryImpl {
   final DioClient client;
 
   PersonsRemoteDatasourceImpl({required this.client});
 
   @override
-  Future getListOfPersons(int quantity) {
-    return client.getDio().get(ApiConst.GET_LIST_OF_PERSONS, queryParameters: {
-      '_quantity': quantity.toString(),
-    });
+  Future<Either<ServerError, ApiResponse>> getListOfPersons(
+      int quantity) async {
+    var response = await call(
+      client.getDio().get(
+        ApiConst.GET_LIST_OF_PERSONS,
+        queryParameters: {
+          KeyConst.paramsQuantity: quantity.toString(),
+        },
+      ),
+    );
+
+    return response.fold(
+      (failure) => Left(failure),
+      (success) {
+        ApiResponse data = ApiResponse.fromJson(success.data);
+        return Right(data);
+      },
+    );
   }
 }
