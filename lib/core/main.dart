23ea85f4868694432296e3af1a6bbf1e0672d859
LuigiGo <luigee.8@gmail.com>
2024-04-06 04:59:32 +0800

    [Core] Initialize main injection container in the main.dart

--- lib/core/main.dart
+++ lib/core/main.dart
@@ -1,4 +1,5 @@
 import 'package:fake_api_exam/core/const/keyconst.dart';
+import 'package:fake_api_exam/core/di/injection.dart';
 import 'package:fake_api_exam/core/utils/environments.dart';
 import 'package:flutter/material.dart';
 
@@ -7,6 +8,7 @@ import 'app.dart';
 void main() async {
   String flavor = const String.fromEnvironment(KeyConst.appFlavor);
   await Environment().initializeEnvironment(flavor);
+  await initDi();
 
   runApp(const MyApp());
 }
