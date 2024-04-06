17f646b5d95a01e475e86d3b6d1fd5ca7cc01f83
LuigiGo <luigee.8@gmail.com>
2024-04-06 17:11:41 +0800

    [DI] Initialize ImageHelpers class

--- lib/core/di/injection.dart
+++ lib/core/di/injection.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:fake_api_exam/core/network/dio_client.dart';
 import 'package:fake_api_exam/core/network/http_logging_interceptor.dart';
+import 'package:fake_api_exam/core/utils/helpers/image_helpers.dart';
 import 'package:fake_api_exam/features/homepage/di/homepage_injection.dart';
 import 'package:get_it/get_it.dart';
 import 'package:logger/logger.dart';
@@ -10,6 +11,7 @@ final inject = GetIt.asNewInstance();
 Future<void> initDi() async {
   inject.registerLazySingleton(() => Dio());
   inject.registerLazySingleton(() => Logger());
+  inject.registerLazySingleton(() => ImageHelpers());
   inject.registerLazySingleton(() => HttpLoggingInterceptor(logger: inject()));
   inject.registerLazySingleton(() => DioClient(
         dio: inject(),
