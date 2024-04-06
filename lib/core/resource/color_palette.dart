a8b1609d404e17d8e25b3f802ad84c4cc06a594b
LuigiGo <luigee.8@gmail.com>
2024-04-06 14:14:39 +0800

    [Resource] Add color palette - this file defines the color palette of the application

---
+++ lib/core/resource/color_palette.dart
@@ -0,0 +1,10 @@
+import 'dart:ui';
+
+const Color _kPrimary = Color(0xFF171F34);
+const Color _kSecondary = Color(0xFF3BEDAE);
+
+class AppColors {
+  static const appBarColor = _kPrimary;
+  static const textColor = _kPrimary;
+  static const colorScheme = _kSecondary;
+}
