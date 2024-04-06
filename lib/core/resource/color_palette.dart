576d235976a1f5e027f94a1cf69290aec6867974
LuigiGo <luigee.8@gmail.com>
2024-04-06 15:36:06 +0800

    [Resource] Add additional colors

--- lib/core/resource/color_palette.dart
+++ lib/core/resource/color_palette.dart
@@ -1,10 +1,16 @@
 import 'dart:ui';
 
 const Color _kPrimary = Color(0xFF171F34);
+
 const Color _kSecondary = Color(0xFF3BEDAE);
+
+const Color _kNetural = Color(0xFFF2F2F5);
 
 class AppColors {
   static const appBarColor = _kPrimary;
+  static const scaffoldBackgroundColor = _kNetural;
   static const textColor = _kPrimary;
-  static const colorScheme = _kSecondary;
+
+  static const primaryColorScheme = _kSecondary;
+  static const circularButtonColor = _kSecondary;
 }
