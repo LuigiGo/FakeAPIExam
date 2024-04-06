1c3c757ce1a31649aff71a5a843e861e2e11970f
LuigiGo <luigee.8@gmail.com>
2024-04-06 17:08:16 +0800

    [Resources] Create AppTypography

---
+++ lib/core/resource/app_typography.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+class AppTypography {
+  static const TextStyle textBodyLarge = TextStyle(fontSize: 18.0);
+  static const TextStyle textBodyMedium = TextStyle(fontSize: 14.0);
+}
