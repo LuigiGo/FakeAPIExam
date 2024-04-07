60dbb6644f4df1c9c3ed54c1736ba40984d2a884
LuigiGo <luigee.8@gmail.com>
2024-04-07 21:41:55 +0800

    [Const] Add additional image paths - Birthday - Email - Gender - Phone

--- lib/core/const/image_const.dart
+++ lib/core/const/image_const.dart
@@ -1,3 +1,7 @@
 class ImageConst {
-  static const defaultImagePlaceholder = 'assets/img_profile.png';
+  static const kAssetProfile = 'assets/img_profile.png';
+  static const kAssetBirthday = 'assets/img_birthday.png';
+  static const kAssetEmail = 'assets/img_email.png';
+  static const kAssetGender = 'assets/img_gender.png';
+  static const kAssetPhone = 'assets/img_phone.png';
 }
