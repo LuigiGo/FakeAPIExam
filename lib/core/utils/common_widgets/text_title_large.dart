33d1abff008dce534f17c386e3c129c1ca010a4f
LuigiGo <luigee.8@gmail.com>
2024-04-07 15:11:29 +0800

    [Common Widgets] Remove unnecessary imports

--- lib/core/utils/common_widgets/text_title_large.dart
+++ lib/core/utils/common_widgets/text_title_large.dart
@@ -2,12 +2,12 @@ import 'package:flutter/material.dart';
 
 import '../../resource/color_palette.dart';
 
-class TextBodyLarge extends StatelessWidget {
+class TextTitleLarge extends StatelessWidget {
   final String? value;
   final Color? textColor;
   final FontWeight? fontWeight;
 
-  const TextBodyLarge({
+  const TextTitleLarge({
     super.key,
     required this.value,
     this.textColor,
@@ -18,8 +18,8 @@ class TextBodyLarge extends StatelessWidget {
   Widget build(BuildContext context) {
     return Text(
       value ?? '',
-      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-            color: textColor ?? AppColors.textColor,
+      style: Theme.of(context).textTheme.titleLarge?.copyWith(
+            color: textColor ?? AppColors.defaultLabelColor,
             fontWeight: fontWeight ?? FontWeight.normal,
           ),
     );
