8649a7843555017ed2fe93895cb6c2568caf613c
LuigiGo <luigee.8@gmail.com>
2024-04-07 15:16:31 +0800

    [Core] Apply ProviderScope

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,14 @@ import 'package:fake_api_exam/core/const/key_const.dart';
 import 'package:fake_api_exam/core/di/injection.dart';
 import 'package:fake_api_exam/core/environments.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'app.dart';
+import 'core/app.dart';
 
 void main() async {
   String flavor = const String.fromEnvironment(KeyConst.appFlavor);
   await Environment().initializeEnvironment(flavor);
   await initDi();
 
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
