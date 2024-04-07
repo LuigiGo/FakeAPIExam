8c054cdc5a457d972d3690d705e8fc96b2a95c85
LuigiGo <luigee.8@gmail.com>
2024-04-07 15:14:11 +0800

    [Common Widgets] Apply Unconstrained Box to support list item sizing

--- lib/core/utils/common_widgets/reusable_card_container.dart
+++ lib/core/utils/common_widgets/reusable_card_container.dart
@@ -1,26 +1,33 @@
 import 'package:flutter/material.dart';
 
 class ReusableCardContainer extends StatelessWidget {
+  final double? cardWidth;
+  final double? cardMargin;
   final Widget? cardContent;
 
   const ReusableCardContainer({
     super.key,
+    this.cardWidth,
+    this.cardMargin,
     this.cardContent,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: const EdgeInsets.all(10.0),
-      child: Card(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(20.0),
+    return UnconstrainedBox(
+      child: Container(
+        width: cardWidth,
+        margin: EdgeInsets.symmetric(vertical: cardMargin ?? 10.0),
+        child: Card(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: cardContent ??
+              const SizedBox(
+                width: double.infinity,
+                height: 120.0,
+              ),
         ),
-        child: cardContent ??
-            const SizedBox(
-              width: double.infinity,
-              height: 120.0,
-            ),
       ),
     );
   }
