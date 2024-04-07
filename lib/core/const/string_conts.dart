ee2a8f57607501821706d0a06e1a0020f5394439
LuigiGo <luigee.8@gmail.com>
2024-04-07 21:04:40 +0800

    [Const] Create StringConst class - this class defines all constant strings

---
+++ lib/core/const/string_conts.dart
@@ -0,0 +1,16 @@
+class StringConst {
+  static const String kAppName = 'FakeApi';
+  static const String kDefaultFontFamily = 'Rubik';
+
+  static const String kSomethingWentWrong = 'Oops! Something went wrong';
+  static const String kAdditionalInformation = 'Additional Information';
+  static const String kPhoneMobile = 'Phone | Mobile';
+  static const String kPersonalEmail = 'Personal | Email';
+  static const String kGender = 'Gender';
+  static const String kBirthday = 'Birthday';
+  static const String kPersonalInformation = 'Personal Information';
+  static const String kLoadMore = 'Load More';
+  static const String kPersonBook = 'Person Book';
+  static const String kTheresNoAvailableData =
+      'Oops! There\'s no available data.';
+}
