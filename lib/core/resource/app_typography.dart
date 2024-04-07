46ccf2e6e621392adb684506c604496e1d588378
LuigiGo <luigee.8@gmail.com>
2024-04-07 15:20:10 +0800

    [Resource] Delete AppTypography class

--- lib/core/resource/app_typography.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-class AppTypography {
-  static const TextStyle textBodyLarge = TextStyle(fontSize: 18.0);
-  static const TextStyle textBodyMedium = TextStyle(fontSize: 14.0);
-}
