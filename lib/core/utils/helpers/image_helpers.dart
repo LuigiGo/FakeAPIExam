fd994fbcd54fd8d6620132d58f8548fd32ad98c4
LuigiGo <luigee.8@gmail.com>
2024-04-07 15:17:23 +0800

    [Utils] Refactor image loading function

--- lib/core/utils/helpers/image_helpers.dart
+++ lib/core/utils/helpers/image_helpers.dart
@@ -4,27 +4,45 @@ import 'package:flutter/material.dart';
 import '../../const/image_const.dart';
 
 class ImageHelpers {
-  Widget loadNetworkImage({String? imgUrl, String? placeholder}) {
-    return ClipOval(
-      child: CachedNetworkImage(
-        imageUrl: imgUrl ?? '',
-        placeholder: (context, url) => const Center(
-          child: SizedBox(
-            width: 20.0,
-            height: 20.0,
-            child: CircularProgressIndicator(),
-          ),
-        ),
-        errorWidget: (context, url, error) {
-          return SizedBox(
-            width: 50.0,
-            height: 50.0,
-            child: Image.asset(
-              placeholder ?? ImageConst.defaultImagePlaceholder,
-            ),
-          );
-        },
+  Widget loadImage({
+    String? imgUrl,
+    String? placeholder,
+    double? width,
+    double? height,
+  }) {
+    return FadeInImage(
+      height: 50,
+      width: 50,
+      image: NetworkImage(imgUrl ?? ''),
+      placeholder: const AssetImage(
+        ImageConst.defaultImagePlaceholder,
       ),
+      imageErrorBuilder: (context, error, stackTrace) {
+        return Image.asset(
+          ImageConst.defaultImagePlaceholder,
+          height: 50,
+          width: 50,
+        );
+      },
     );
+    // return ClipOval(
+    //   child: CachedNetworkImage(
+    //     imageUrl: imgUrl ?? '',
+    //     placeholder: (context, url) => const Center(
+    //       child: SizedBox(
+    //         width: 30.0,
+    //         height: 30.0,
+    //         child: CircularProgressIndicator(),
+    //       ),
+    //     ),
+    //     errorWidget: (context, url, error) {
+    //       return SizedBox(
+    //         width: width ?? 50.0,
+    //         height: height ?? 50.0,
+    //         child: Icon(Icons.person),
+    //       );
+    //     },
+    //   ),
+    // );
   }
 }
