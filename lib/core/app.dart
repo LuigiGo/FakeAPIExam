ebd0d9dca60b99414c9115b77060cbb0a6192c87
LuigiGo <luigee.8@gmail.com>
2024-04-07 15:20:35 +0800

    [Core] Apply textTheme

--- lib/core/app.dart
+++ lib/core/app.dart
@@ -1,6 +1,6 @@
-import 'package:fake_api_exam/core/resource/app_typography.dart';
 import 'package:fake_api_exam/core/resource/color_palette.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_builder/responsive_builder.dart';
 
 import 'const/routes_const.dart';
 import 'navigation/route_generator.dart';
@@ -10,25 +10,25 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: false,
-        appBarTheme: const AppBarTheme(
-          centerTitle: true,
-          backgroundColor: AppColors.appBarColor,
-        ),
-        colorScheme: const ColorScheme.light(
-          primary: AppColors.primaryColorScheme,
-        ),
-        scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor,
-        textTheme: const TextTheme(
-          bodyLarge: AppTypography.textBodyLarge,
-          bodyMedium: AppTypography.textBodyMedium,
-        ),
-      ),
-      initialRoute: RoutesConst.root,
-      onGenerateRoute: RouteGenerator.generateRoute,
+    return ResponsiveApp(
+      builder: (BuildContext context) {
+        return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            useMaterial3: false,
+            appBarTheme: const AppBarTheme(
+              centerTitle: true,
+              backgroundColor: AppColors.appBarColor,
+            ),
+            colorScheme: const ColorScheme.light(
+              primary: AppColors.primaryColorScheme,
+            ),
+            scaffoldBackgroundColor: AppColors.scaffoldBackgroundColor,
+          ),
+          initialRoute: RoutesConst.root,
+          onGenerateRoute: RouteGenerator.generateRoute,
+        );
+      },
     );
   }
 }
