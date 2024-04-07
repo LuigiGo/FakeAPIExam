e9d72bc6f3444bc079d4dbfda8c607e1e7510825
LuigiGo <luigee.8@gmail.com>
2024-04-07 15:19:52 +0800

    [Resource] Refactor color palette

--- lib/core/resource/color_palette.dart
+++ lib/core/resource/color_palette.dart
@@ -1,16 +1,27 @@
 import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
 const Color _kPrimary = Color(0xFF171F34);
 
 const Color _kSecondary = Color(0xFF3BEDAE);
 
 const Color _kNetural = Color(0xFFF2F2F5);
+const Color _kNetural1 = Color(0xFFEAEAEB);
+const Color _kNetural2 = Color(0x56BEBEBE);
+const Color _kNetural3 = Color(0xFFA6A6A6);
 
 class AppColors {
   static const appBarColor = _kPrimary;
-  static const scaffoldBackgroundColor = _kNetural;
-  static const textColor = _kPrimary;
+  static const defaultLabelColor = _kPrimary;
+  static const alternativeLabelColor = _kNetural3;
 
   static const primaryColorScheme = _kSecondary;
   static const circularButtonColor = _kSecondary;
+  static const defaultButtonColor = _kSecondary;
+
+  static const scaffoldBackgroundColor = _kNetural1;
+  static const informationPageBackgroundColor = _kNetural;
+  static const dottedContainerColor = Colors.white;
+  static const dottedBorderColor = _kNetural2;
 }
